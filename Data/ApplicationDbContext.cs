9b903c65e398205df25a736c4d0c085904375111
Sagor31h2 <sagor31h2@gmail.com>
2021-11-19 16:29:41 +0600

    Ading model to database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
-
+using pocketBook.Models;
 namespace pocketBook.Data
 {
     public class ApplicationDbContext : DbContext
+
     {
+        public virtual DbSet<User> Users { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
