387b1d57b47e9d3b59220d53120832ae7197ea1a
Sagor31h2 <sagor31h2@gmail.com>
2021-11-19 17:17:29 +0600

    Update database

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace PocketBook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("DefaultConnection"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
